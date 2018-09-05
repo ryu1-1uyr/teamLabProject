ec51e1529d02aad158cec3cb25e3951518f76b9c
s17001 <s17001@std.it-college.ac.jp>
2018-09-05 16:06:50 +0900

    .gitignore is now working

--- testApp-api/app/models/recommended_item.rb
+++
@@ -1,2 +0,0 @@
-class RecommendedItem < ApplicationRecord
-end
--- testApp-api/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :recommended_items
-  resources :categories
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-end
