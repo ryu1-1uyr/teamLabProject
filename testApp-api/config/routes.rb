b02cc653170b48c5710776dd6fe7461087c49795
s17001 <s17001@std.it-college.ac.jp>
2018-09-06 18:53:31 +0900

    make administrator API

--- testApp-api/config/routes.rb
+++ testApp-api/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :administrators
   get '/search' => "search#product"
   post '/search' => "search#test"
 
--- testApp-api/db/schema.rb
+++ testApp-api/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_044826) do
+ActiveRecord::Schema.define(version: 2018_09_06_095104) do
+
+  create_table "administrators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "pwd"
+    t.string "permission"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
