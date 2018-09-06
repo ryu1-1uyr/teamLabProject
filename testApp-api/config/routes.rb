b735231524a9da87ec4bf7c89297944728282bfd
s17001 <s17001@std.it-college.ac.jp>
2018-09-06 18:57:18 +0900

    無駄な行の削除

--- testApp-api/config/routes.rb
+++ testApp-api/config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  
-  resources :administrators
+
   get '/search' => "search#product"
   post '/search' => "search#test"
 
-
   get '/tester' => "tester#order"
+  resources :administrators
   resources :orders
   resources :recommended_items
   resources :categories
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  
 
 end
